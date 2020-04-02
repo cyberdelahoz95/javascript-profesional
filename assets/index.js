f4019b7e9182cb6546fa49092c5e98651a62f214
Henry de la Hoz <cyberdelahoz95@gmail.com>
2020-04-02 13:20:23 -0500

    Separating MediaPlayer as a module

--- assets/index.js
+++ assets/index.js
@@ -1,18 +1,9 @@
+import MediaPlayer from './MediaPlayer.js';
+
 const video = document.querySelector('video');
 const button = document.querySelector('button');   
-function MediaPlayer(config){
-    this.media = config.el
-}
-MediaPlayer.prototype.play = function () {
-    this.media.play();
-}
-MediaPlayer.prototype.pause = function() {
-  this.media.pause()
-}
-MediaPlayer.prototype.paused = function() {
-  returnthis.media.paused
-}
 const player = new MediaPlayer({el:video});
+
 button.onclick = () => {
   player.paused()
   ? player.play()
