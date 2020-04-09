6d1538e50d3dde4f5775804b43e2a86879692d5e
Henry de la Hoz <cyberdelahoz95@gmail.com>
2020-04-09 13:15:01 -0500

    Implementing attribute to MediaPlayer to accept plugins

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,14 +1,28 @@
-function MediaPlayer(config){
-    this.media = config.el
+function MediaPlayer(config) {
+	this.media = config.el;
+	this.plugins = config.plugins || [];
+
+	this._initPlugins();
 }
 MediaPlayer.prototype.play = function () {
-    this.media.play();
-}
-MediaPlayer.prototype.pause = function() {
-  this.media.pause()
-}
-MediaPlayer.prototype.paused = function() {
-  returnthis.media.paused
-}
+	this.media.play();
+};
+MediaPlayer.prototype.pause = function () {
+	this.media.pause();
+};
+MediaPlayer.prototype.paused = function () {
+	return this.media.paused;
+};
+MediaPlayer.prototype.mute = function () {
+	this.media.muted = true;
+};
+MediaPlayer.prototype.unmute = function () {
+	this.media.muted = false;
+};
+MediaPlayer.prototype._initPlugins = function () {
+	this.plugins.forEach((plugin) => {
+		plugin.run();
+	});
+};
 
 export default MediaPlayer;
