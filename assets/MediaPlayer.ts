11cd1afea918d6b7a1f09ebd3d135e6d56c81627
Henry de la Hoz <cyberdelahoz95@gmail.com>
2020-05-23 13:19:07 -0500

    Ads plugin implemented

--- assets/MediaPlayer.ts
+++ assets/MediaPlayer.ts
@@ -1,9 +1,11 @@
 class MediaPlayer {
 	media: HTMLMediaElement;
 	plugins: Array<any>;
+	container: HTMLElement;
 	constructor(config) {
 		this.media = config.el;
 		this.plugins = config.plugins || [];
+		this.initPlayer();
 		this.initPlugins();
 	}
 	play() {
@@ -20,6 +22,12 @@ class MediaPlayer {
 	}
 	unmute() {
 		this.media.muted = false;
+	}
+	initPlayer() {
+		this.container = document.createElement('div');
+		this.container.style.position = 'relative';
+		this.media.parentNode.insertBefore(this.container, this.media);
+		this.container.appendChild(this.media);
 	}
 	private initPlugins() {
 		this.plugins.forEach((plugin) => {
--- assets/index.ts
+++ assets/index.ts
@@ -1,6 +1,7 @@
 import MediaPlayer from './MediaPlayer';
 import AutoPlay from './plugins/AutoPlay';
 import AutoPause from './plugins/AutoPause';
+import Ads from './plugins/Ads';
 
 const video = document.querySelector('video');
 const button: HTMLElement = document.querySelector('button');
@@ -8,7 +9,7 @@ const muteButton: HTMLElement = document.querySelector('#muteButton');
 
 const player = new MediaPlayer({
 	el: video,
-	plugins: [new AutoPlay(), new AutoPause()],
+	plugins: [new AutoPlay(), new AutoPause(), new Ads()],
 });
 
 button.onclick = () => {
